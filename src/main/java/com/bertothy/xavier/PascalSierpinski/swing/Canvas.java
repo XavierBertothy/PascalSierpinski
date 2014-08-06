6b07c2e4ceb44c823cc696ee5dbc525c42fbd049
Xavier Bertothy <xavier.bertothy@gmail.com>
2014-08-06 21:55:26 +0100

    JOptionPane added for incorrect level and number.

--- src/main/java/com/bertothy/xavier/PascalSierpinski/swing/Canvas.java
+++ src/main/java/com/bertothy/xavier/PascalSierpinski/swing/Canvas.java
@@ -7,11 +7,13 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
@@ -20,22 +22,22 @@ import org.apache.log4j.Logger;
 
 import com.bertothy.xavier.PascalSierpinski.model.Action;
 import com.bertothy.xavier.PascalSierpinski.model.Choice;
+import com.bertothy.xavier.PascalSierpinski.model.Triangle;
 
 public class Canvas {
 
-	public static final Logger log = Logger.getLogger(Canvas.class.getName());
+	public static final Logger LOG = Logger.getLogger(Canvas.class.getName());
 
-	public static final int commandWidth = 450;
-	
-	public static final int commandHeight = 900;
-	public static final int triangleWidth = 1350;
-	public static final int triangleHeight = 900;
-	public static final Color backgroundColor = Color.BLACK;
-	public static final Color shapeColor = Color.DARK_GRAY;
-	public static final Color textColor = Color.WHITE;
-	public static final Color[] colorArray = { Color.RED, Color.ORANGE, Color.YELLOW,
+	public static final int COMMANDWIDTH = 450;	
+	public static final int COMMANDHEIGHT = 900;
+	public static final int TRIANGLEWIDTH = 1350;
+	public static final int TRIANGLEHEIGHT = 900;
+	public static final Color BACKGROUNDCOLOR = Color.BLACK;
+	public static final Color SHAPECOLOR = Color.DARK_GRAY;
+	public static final Color TEXTCOLOR = Color.WHITE;
+	public static final Color[] COLORARRAY = { Color.RED, Color.ORANGE, Color.YELLOW,
 		Color.GREEN, Color.CYAN, Color.BLUE, Color.PINK, Color.MAGENTA };
-	public static final CellColorRenderer cellColorRenderer = new CellColorRenderer();
+	public static final CellColorRenderer CELLCOLORRENDER = new CellColorRenderer();
 	
 	public static String level = "10";
 	public static ArrayList<Choice> choiceList = new ArrayList<Choice>();
@@ -48,7 +50,7 @@ public class Canvas {
 
 		JFrame frame = new JFrame("Pascal Sierpinski");
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.setSize(triangleWidth+commandWidth, triangleHeight);
+		frame.setSize(TRIANGLEWIDTH+COMMANDWIDTH, TRIANGLEHEIGHT);
 		Container container = frame.getContentPane();
 		container.setLayout(new BorderLayout());
 
@@ -65,23 +67,43 @@ public class Canvas {
 		JButton submitButton = new JButton("Submit");
 		submitButton.addActionListener(e -> {
 			level = levelField.getText();
+			
+			if (level.trim().equals("") || !isNumber(level) || !isLevelCorrect(level)){
+				JOptionPane.showMessageDialog(frame,
+					    "Level cannot be empty.\n" + "Level must be a positive number such as :\n" + 
+					    Triangle.MINLEVEL + " <= Level <= " + Triangle.MAXLEVEL + "\n" 
+					    + "Value entered: " + level,
+					    "Level error",
+					    JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+			
 			choiceList = new ArrayList<Choice>();
 			for(int x = 0; x < choicesPanel.getComponentCount(); x++){
 				JPanel row = (JPanel)choicesPanel.getComponent(x);
 				Choice choice = new Choice();
 				choice.setAction(Action.valueOf((String)((JComboBox<String>)row.getComponent(0)).getSelectedItem()));
 				JPanel numberPanel = (JPanel)row.getComponent(1);
+				String number = ((JTextField)numberPanel.getComponent(1)).getText();
+				if (number.trim().equals("") || !isNumber(number)){
+					JOptionPane.showMessageDialog(frame,
+						    "Number cannot be empty.\n" + "Number must be positive.\n" 
+						    + "Value entered: " + number,
+						    "Number error",
+						    JOptionPane.ERROR_MESSAGE);
+					return;
+				}
 				choice.setNumber(((JTextField)numberPanel.getComponent(1)).getText());
 				JPanel colorPanel = (JPanel)row.getComponent(2);
 				choice.setColor((Color)((JComboBox<Color>)colorPanel.getComponent(1)).getSelectedItem());
 				choiceList.add(choice);
 			}
 			
-			log.info("level=" + level);
+			LOG.info("level=" + level);
 			if(choiceList!=null)
 				for(Choice choice:choiceList)
-					log.info(choice);
-			log.info("submit");
+					LOG.info(choice);
+			LOG.info("submit");
 			trianglePanel.setLevel(level);
 			trianglePanel.setChoiceList(choiceList);
 			trianglePanel.revalidate();  
@@ -104,8 +126,8 @@ public class Canvas {
 			numbersPanel.add(numbersField, BorderLayout.CENTER);
 
 			JLabel colorLabel = new JLabel("Colors: ");
-			JComboBox<Color> colorComboBox = new JComboBox<>(colorArray);
-			colorComboBox.setRenderer(cellColorRenderer);
+			JComboBox<Color> colorComboBox = new JComboBox<>(COLORARRAY);
+			colorComboBox.setRenderer(CELLCOLORRENDER);
 
 			JPanel colorPanel = new JPanel();
 			colorPanel.setLayout(new BorderLayout());
@@ -151,22 +173,31 @@ public class Canvas {
 		commandPanel.setLayout(new BorderLayout());
 		commandPanel.add(generalPanel, BorderLayout.NORTH);
 		commandPanel.add(choicesPanel, BorderLayout.CENTER);
-		commandPanel.setPreferredSize(new Dimension(commandWidth, commandHeight));
+		commandPanel.setPreferredSize(new Dimension(COMMANDWIDTH, COMMANDHEIGHT));
 
 		container.add(commandPanel, BorderLayout.WEST);
 
 		//Triangle panel
 		trianglePanel = new TrianglePanel();
-		trianglePanel.setBackgroundColor(backgroundColor);
-		trianglePanel.setShapeColor(shapeColor);
-		trianglePanel.setTextColor(textColor);
-		trianglePanel.setWidth(triangleWidth);
-		trianglePanel.setHeight(triangleHeight);
+		trianglePanel.setBackgroundColor(BACKGROUNDCOLOR);
+		trianglePanel.setShapeColor(SHAPECOLOR);
+		trianglePanel.setTextColor(TEXTCOLOR);
+		trianglePanel.setWidth(TRIANGLEWIDTH);
+		trianglePanel.setHeight(TRIANGLEHEIGHT);
 		trianglePanel.setLevel(level);
 		trianglePanel.setChoiceList(choiceList);
 		container.add(trianglePanel, BorderLayout.CENTER);
 
 		frame.setVisible(true);
+	}
+
+	private static boolean isNumber(String level) {
+		return Pattern.matches("\\d+", level.trim());
+	}
+
+	private static boolean isLevelCorrect(String level) {
+		int levelInt = Integer.valueOf(level);
+		return levelInt>=Triangle.MINLEVEL && levelInt<=Triangle.MAXLEVEL;
 	}
 
 }
